b2c79d4cd81e5566600c3f391a0f394f12728c4e
Emily Raper <emily@ebs-er-10-145-4-137.wireless.umn.edu>
2019-03-30 17:51:44 -0500

    Working on making keys work

--- liri.js
+++ liri.js
@@ -8,18 +8,22 @@ inquirer.prompt([
       name: "song",
       message: "Welcome to Liri! This app allows you to search Spotify for a song and get the artist's name, the song's name, a preview link of the song on Spotify, and the album that the song is from.  Please begin your search by entering: node liri.js spotify-this-song '<song name here>'  into the command line."
     },
-]).then(function(user) {
-    console.log(user.song);
-});
-   // var Spotify = require('node-spotify-api');
-   // spotify.search({ type: 'track', query: user.song }, function(err, data) {
-       // if (err) {
-          //return console.log('Error occurred: ' + err);
-     //   }     
-     // console.log(data);
-   // }
-   // )} 
-//);
+    ]).then(function(data) {
+    var spotify = new spotify({
+        id: c08c180f0d24123a962acddf546174f,
+        secret: c08c180f0d24123a962acddf546174f
+      });
+       
+      spotify.search({ type: 'track', query: user.song }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+    })
+
+
 
 
 
