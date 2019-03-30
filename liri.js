c935dae3d7c2c3dbd97fe2910b1cf0b21df79c25
Emily Raper <emily@ebs-er-10-145-4-137.wireless.umn.edu>
2019-03-30 17:22:59 -0500

    Inquirer added

--- liri.js
+++ liri.js
@@ -1,5 +1,29 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var inquirer = require("inquirer");
+inquirer.prompt([
+
+    {
+      type: "input",
+      name: "song",
+      message: "Welcome to Liri! This app allows you to search Spotify for a song and get the artist's name, the song's name, a preview link of the song on Spotify, and the album that the song is from.  Please begin your search by entering: node liri.js spotify-this-song '<song name here>'  into the command line."
+    },
+]).then(function(user) {
+    console.log(user.song);
+});
+   // var Spotify = require('node-spotify-api');
+   // spotify.search({ type: 'track', query: user.song }, function(err, data) {
+       // if (err) {
+          //return console.log('Error occurred: ' + err);
+     //   }     
+     // console.log(data);
+   // }
+   // )} 
+//);
+
+
+
+
+
 
 
