d0724b63a8c76023b7c064c7f63937b008e1f1d6
Emily Raper <emily@stp-er-10-129-225-167.wireless.umn.edu>
2019-04-01 18:48:06 -0500

    Working command and input

--- liri.js
+++ liri.js
@@ -1,27 +1,31 @@
-require("dotenv").config();
-var keys = require("./keys.js");
+var dotenv = require("dotenv").config();
 var inquirer = require("inquirer");
-inquirer.prompt([
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var command = process.argv[2];
+var input = process.argv[3];
+console.log("---------You ran command " + command + " \nwith input " + input+"-----------");
 
-    {
-      type: "input",
-      name: "song",
-      message: "Welcome to Liri! This app allows you to search Spotify for a song and get the artist's name, the song's name, a preview link of the song on Spotify, and the album that the song is from.  Please begin your search by entering: node liri.js spotify-this-song '<song name here>'  into the command line."
-    },
-    ]).then(function(data) {
-    var spotify = new spotify({
-        id: c08c180f0d24123a962acddf546174f,
-        secret: c08c180f0d24123a962acddf546174f
-      });
+// inquirer.prompt([
+
+//     {
+//       type: "input",
+//       name: "song",
+//       message: "Welcome to Liri! This app allows you to search Spotify for a song and get the artist's name, the song's name, a preview link of the song on Spotify, and the album that the song is from.  Please begin your search by entering: node liri.js spotify-this-song '<song name here>'  into the command line."
+//     },
+//     ]).then(function(data) {
+    var spotify = new Spotify(
+      keys.spotify
+    );
        
-      spotify.search({ type: 'track', query: user.song }, function(err, data) {
+      spotify.search({ type: command, query: input }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
        
       console.log(data); 
       });
-    })
+  
 
 
 
