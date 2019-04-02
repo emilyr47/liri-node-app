daa0713ffcfb3ea7638a34e0ae76766de97c32b1
Emily Raper <emily@stp-er-10-129-225-167.wireless.umn.edu>
2019-04-01 20:25:07 -0500

    More high-level commands added and if/ else statements

--- liri.js
+++ liri.js
@@ -6,26 +6,70 @@ var command = process.argv[2];
 var input = process.argv[3];
 console.log("---------You ran command " + command + " \nwith input " + input+"-----------");
 
-// inquirer.prompt([
+// Concert-this Command: node liri.js concert-this <artist/band name here>
 
-//     {
-//       type: "input",
-//       name: "song",
-//       message: "Welcome to Liri! This app allows you to search Spotify for a song and get the artist's name, the song's name, a preview link of the song on Spotify, and the album that the song is from.  Please begin your search by entering: node liri.js spotify-this-song '<song name here>'  into the command line."
-//     },
-//     ]).then(function(data) {
-    var spotify = new Spotify(
-      keys.spotify
-    );
-       
-      spotify.search({ type: command, query: input }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data); 
-      });
-  
+function concertThis() {
+  if (command === "concert-this") {
+  var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+  console.log("");
+  }
+}
+
+// Spotify-this-Song Command: node liri.js spotify-this-song '<song name here>'
+function spotifyThis() {
+if (command === "spotify-this-song") {
+  var spotify = new Spotify(
+    keys.spotify
+  );
+     
+    spotify.search({ type: command, query: input }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+     
+    console.log(data); 
+    });
+}
+}
+
+
+//Movie-this Command: node liri.js movie-this '<movie name here>'
+function movieThis() {
+if (command === "movie-this") {
+  var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+  console.log("");
+  }
+}
+
+
+// Do-What-It-Says Command: node liri.js do-what-it-says
+function doWhatItSays() {
+if (command === "do-what-it-says") {
+  var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+  console.log("");
+  }
+}
+
+if (command === "do-what-it-says") {
+  doWhatItSays();
+}
+
+else if (command === "movie-this") {
+  movieThis();
+}
+
+else if (command === "movie-this") {
+  movieThis();
+}
+
+else if (command === "spotify-this-song") {
+  spotifyThis();
+}
+
+else {
+  console.log("I'm sorry, I did not catch that.  Please enter an actual command");
+}
+
 
 
 
